c5391bfda246adda5dcae34fbbfea603f8868640
David <david@cortx.com>
2018-06-28 12:27:55 -0400

    added isValidApiKey function

--- perfecttense.js
+++ perfecttense.js
@@ -29,25 +29,53 @@ this.verbose = true
 this.persist = true
 
 /**
- * Initialize PT client
+ * Test the validity of the parameter API key
  *     
- * @param {string} config.appKey                           App key assigned to this registered application [insert link to register app]
- * @param {boolean} config.verbose=true                    Set verbose console output for debugging purposes
- * @param {boolean} config.persist=true                    Optionally persist corrections (Help Perfect Tense get better!)
- * @param {Object} config.options={}                       Optional default options such as protected text (see API documentation)
- * @param {Object} config.responseType=["rulesApplied"]    Optional array of response types (see API documentation) 
+ * @param {string} apiKey           The API key to test
+ *
+ * @return {boolean}                True if the API key is valid, else false
  */
-this.initialize = function(config) {
-    this.appKey = config.appKey || "",
-        this.verbose = config.verbose,
-        this.persist = config.persist,
-        this.options = config.options,
-        this.responseType = config.responseType || this.ALL_RESPONSE_TYPES
+this.apiKeyIsValid = function(apiKey) {
+
+    const payload = {
+        method: 'GET',
+        url: PT_BASE_URL + "/testAuth",
+        //data: {},
+        headers: {
+            'Authorization': apiKey,
+            'Content-Type': 'application/json'
+        }
+    }
+
+    console.log("Making request: ")
+    console.log(payload)
+
+    function apiSuccess(response) {
+        return true
+    }
+
+    function apiFailure(response) {
+        return false
+    }
+
+    return axios(payload).then(apiSuccess, apiFailure)
 }
 
 /*********************************************************************
                 Interaction With Perfect Tense API
 **********************************************************************/
+
+/*
+ * Submit text to Perfect Tense, receiving specified responseTypes in result.
+ *
+ * @param {string} text             Text to be submitted
+ * @param {string} apiKey           The user's API key
+ * @param {Object} options          Options such as protected text. Defaults to options set during initialization
+ * @param {Object} responseType     Array of response types. Defaults to responseType set during initialization
+ *
+ * @return {Object}                 Promise containing the job result
+ */
+
 
 /*
  * Submit text to Perfect Tense, receiving specified responseTypes in result.
